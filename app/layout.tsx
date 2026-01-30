b74271bf44047cd24b8bb3d0056b1022d9bac03d
Carsten Rheidt <carstenrheidt@MBP-von-Carsten.fritz.box>
2026-01-30 09:54:32 +0100

    fix(ui): add global Toaster component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { IOSplashScreens } from "@/components/IOSplashScreens";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -45,6 +46,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
